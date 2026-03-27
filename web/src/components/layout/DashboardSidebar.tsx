61bc4de5279bf56e8c3c2cb83ce303c5cd66c527
Daniel Farr <daniel@aptivada.com>
2026-03-27 10:14:45 -0600

    stopping resizing

--- web/src/components/layout/DashboardSidebar.tsx
+++ web/src/components/layout/DashboardSidebar.tsx
@@ -1,7 +1,9 @@
 import IconExpandLeft from "~icons/mdi/arrow-expand-left";
 import IconCollapseRight from "~icons/mdi/arrow-collapse-right";
 import { useSidebarStore } from "../../store/sidebarStore";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
+import { useDraggable, DragDropProvider } from "@dnd-kit/react";
+import { RestrictToHorizontalAxis } from "@dnd-kit/abstract/modifiers";
 
 export type DashboardSidebarProps = React.PropsWithChildren<{
   isOpen: boolean;
@@ -11,6 +13,58 @@ export const DashboardSidebar = ({
   children,
   isOpen,
 }: DashboardSidebarProps) => {
+  const sidebar = useSidebarStore();
+  const initialWidthRef = useRef(sidebar.width);
+  return (
+    <DragDropProvider
+      onDragStart={() => {
+        initialWidthRef.current = sidebar.width;
+      }}
+      onDragMove={(b) => {
+        // const { position } = b.operation;
+        // console.log("Current position:", {
+        //   current: position.current,
+        //   initial: position.initial,
+        //   transformX: b.operation.transform.x,
+        // });
+
+        const toSet = Math.max(
+          Math.min(
+            initialWidthRef.current - b.operation.transform.x,
+            window.innerWidth * 0.8,
+          ),
+          450,
+        );
+        console.log(
+          "setting",
+          toSet,
+          "from current",
+          initialWidthRef.current,
+          "-",
+          b.operation.transform.x,
+        );
+        sidebar.setWidth(toSet);
+      }}
+      onDragEnd={(c) => {
+        console.log(`Dropped`, c);
+      }}
+    >
+      <DashboardSidebarBody isOpen={isOpen}>{children}</DashboardSidebarBody>
+    </DragDropProvider>
+  );
+};
+
+export const DashboardSidebarBody = ({
+  children,
+  isOpen,
+}: DashboardSidebarProps) => {
+  const handleRef = useRef<HTMLButtonElement>(null);
+
+  const { ref } = useDraggable({
+    id: "dashboard-sidebar",
+    modifiers: [RestrictToHorizontalAxis],
+  });
+
   const sidebar = useSidebarStore();
 
   const onClickToggle = useCallback(() => {
@@ -25,6 +79,8 @@ export const DashboardSidebar = ({
 
   return (
     <div
+      ref={ref}
+      id="dashboard-sidebar"
       className={`dashboard-sidebar fixed z-100 top-0 h-screen transition transition-[right] min-w-[300px] max-w-[80vw] shadow ease-out w-[var(--sidebar-width)] ${isOpen ? "right-0" : `right-[var(--sidebar-right-open)]`}`}
       style={{
         "--sidebar-width": `${width}px`,
@@ -33,6 +89,7 @@ export const DashboardSidebar = ({
     >
       <div className="right-arrow absolute top-[100px] z-200 left-[-40px]">
         <button
+          ref={handleRef}
           className="p-2 bg-slate-500 text-white border border-slate-600 flex items-center justify-center h-10"
           onClick={onClickToggle}
         >
