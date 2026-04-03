3b5149d21ccc0affddbd89cc24c93f75da4aace6
Daniel Farr <daniel@aptivada.com>
2026-04-03 17:32:11 -0600

    ready for grid overlay

--- web/src/components/plot/SceneRenderer/SceneCard.tsx
+++ web/src/components/plot/SceneRenderer/SceneCard.tsx
@@ -42,7 +42,7 @@ export const SceneCard = memo(
       ref: containerRef,
       // isDropTarget,
       isDragSource,
-      // isDragging,
+      isDragging,
       // isDropping,
       handleRef,
     } = useSortable({
@@ -86,7 +86,7 @@ export const SceneCard = memo(
         style={themeStyles}
         data-r={sceneIndex}
         data-c={plotIndex}
-        className={`card card--empty group relative p-[var(--card-padding)] w-[var(--column-width)] min-h-[var(--card-min-height)] border border-[var(--plot-color-soft)] radius-2 h-full ${isDropTarget && !isDragSource ? `text-white border-purple-300 bg-purple-900 shadow-lg` : `bg-[var(--plot-color)]  border-[var(--plot-color)] text-[var(--plot-text)]`}`}
+        className={`card card--empty group relative p-[var(--card-padding)] w-[var(--column-width)] min-h-[var(--card-min-height)] border border-[var(--plot-color-soft)] radius-2 h-full ${isDragging ? `scale-80` : `scale-100`} ${isDropTarget && !isDragSource ? `text-white border-purple-300 bg-purple-900 shadow-lg` : `bg-[var(--plot-color)]  border-[var(--plot-color)] text-[var(--plot-text)]`}`}
       >
         <ButtonGroup className="absolute right-1 top-1 opacity-0 transition-opacity duration-200 group-hover:opacity-100 z-20">
           <Button
