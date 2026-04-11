ab3dcf084424f3bb58a92fa21f4467cc94673328
Daniel Farr <daniel@aptivada.com>
2026-04-11 15:18:10 -0600

    ready to build virtuoso sidebar

--- web/src/components/story/ListView.tsx
+++ web/src/components/story/ListView.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
 import type { Plot } from "../../api/types";
 import {
   entryIsScene,
@@ -44,6 +45,8 @@ export const ListView = ({ storyId, plots }: ListViewProps) => {
     [plots, sections],
   );
 
+  const [isScrolling, setIsScrolling] = useState(false);
+
   if (orderedScenes.length === 0) {
     return (
       <div className="rounded-2xl border border-slate-200 bg-white p-6 text-sm text-slate-600">
@@ -57,32 +60,63 @@ export const ListView = ({ storyId, plots }: ListViewProps) => {
       className="y-scroller overflow-y-auto h-[var(--grid-height)]"
       style={{ "--grid-height": `calc(100vh - 61px)` }}
     >
-      <div className="sticky top-0 z-155 mx-auto max-w-[1000px]">
-        <StoryFiltersBar plots={plots} tags={tags} characters={characters} />
-      </div>
+      <div className="flex mx-auto gap-x-4 px-6">
+        <div className="shrink w-[300px] sticky top-0 pt-6 pl-6 bg-white h-[var(--grid-height)] shadow-md">
+          <Virtuoso
+            style={{ height: "100%" }}
+            context={{ isScrolling }}
+            isScrolling={setIsScrolling}
+            data={orderedScenes}
+            itemContent={(_index, entry, _data) => {
+              if (entryIsScene(entry)) {
+                const { scene } = entry;
+                return (
+                  <div key={scene.id} className="list-miniview-scene">
+                    {scene.title}
+                  </div>
+                );
+              }
+              const { section } = entry;
+              return (
+                <div key={section.id} className="list-miniview-section">
+                  {section.title}
+                </div>
+              );
+            }}
+          />
+        </div>
+        <div className="grow max-w-[1000px]">
+          <div className="sticky top-0 z-155">
+            <StoryFiltersBar
+              plots={plots}
+              tags={tags}
+              characters={characters}
+            />
+          </div>
+          <div className="flex my-6 flex-col gap-y-2">
+            {orderedScenes.map((entry) => {
+              if (entryIsScene(entry)) {
+                const { scene, plot } = entry;
+                return (
+                  <ListViewScene
+                    key={scene.id}
+                    scene={scene}
+                    plot={plot}
+                    tags={tags}
+                    characters={characters}
+                    filterVisibilityMode={filterVisibilityMode}
+                    isFilterExcluded={
+                      hasFilters && !includedSceneIdSet.has(scene.id)
+                    }
+                  />
+                );
+              }
 
-      <div className="flex my-6 flex-col mx-auto max-w-[1000px] gap-y-2">
-        {orderedScenes.map((entry) => {
-          if (entryIsScene(entry)) {
-            const { scene, plot } = entry;
-            return (
-              <ListViewScene
-                key={scene.id}
-                scene={scene}
-                plot={plot}
-                tags={tags}
-                characters={characters}
-                filterVisibilityMode={filterVisibilityMode}
-                isFilterExcluded={
-                  hasFilters && !includedSceneIdSet.has(scene.id)
-                }
-              />
-            );
-          }
-
-          const { section } = entry;
-          return <ListViewSection section={section} />;
-        })}
+              const { section } = entry;
+              return <ListViewSection section={section} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
