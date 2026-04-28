f27c4cab3504ab503e54e55dffab465294868db5
Daniel Farr <daniel@aptivada.com>
2026-04-27 20:40:35 -0600

    standardize colors

--- express/src/models/colors.ts
+++ express/src/models/colors.ts
@@ -22,16 +22,16 @@ export interface ColorDefinition extends BaseModelBlueprint {
 export type ColorDocument = ModelDocument<ColorDefinition>;
 
 const DEFAULT_COLORS: string[] = [
-  "#ef4444",
-  "#f97316",
-  "#eab308",
-  "#22c55e",
-  "#14b8a6",
-  "#3b82f6",
   "#8b5cf6",
+  "#3b82f6",
+  "#14b8a6",
+  "#22c55e",
+  "#eab308",
+  "#f97316",
+  "#ef4444",
   "#ec4899",
   "#64748b",
-  "#f59e0b",
+  "#202c3d",
 ];
 
 export const getColorsCollection = (): Collection<ColorDocument> =>
--- web/src/types/color.ts
+++ web/src/types/color.ts
@@ -6,14 +6,14 @@ export interface StoryColor {
 }
 
 export const DEFAULT_PALETTE_COLORS: string[] = [
-  "#ef4444",
-  "#f97316",
-  "#eab308",
-  "#22c55e",
-  "#14b8a6",
-  "#3b82f6",
   "#8b5cf6",
+  "#3b82f6",
+  "#14b8a6",
+  "#22c55e",
+  "#eab308",
+  "#f97316",
+  "#ef4444",
   "#ec4899",
   "#64748b",
-  "#f59e0b",
+  "#202c3d",
 ];
